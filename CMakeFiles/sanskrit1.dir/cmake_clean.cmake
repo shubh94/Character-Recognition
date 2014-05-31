8bdc1fc73957e882a79715b4b1b05fbdac79b70a
luckysahani <luckythegreat4@gmail.com>
2014-06-01 00:25:48 +0530

    ocr commit 1

---
+++ CMakeFiles/sanskrit1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sanskrit1.dir/sanskrit.cpp.o"
+  "sanskrit1.pdb"
+  "sanskrit1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sanskrit1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ scan .cpp
@@ -0,0 +1,74 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+/**
+ * @function main
+ */
+int main( int argc, char** argv )
+{
+  Mat src, dst;
+
+  /// Load image
+  src = imread( argv[1], 1 );
+
+  if( !src.data )
+    { return -1; }
+	
+	int start=0,end=0,prev=255,sum;
+
+	cv::imshow("Input",src);
+for(int i=0; i<src.rows; i++)
+ { 
+ 	sum=0;
+ 	for(int j=0; j<src.cols; j++)
+    {
+      	if(src.at<uchar>(i,j)<120)
+      	{
+      		sum=sum+0;
+      	}
+      	else
+      	{
+      		sum=sum+255;
+      	}
+     }
+     sum=sum/src.cols;
+     cout<<i<<" "<<endl;
+     if(sum==0)
+     	cout<<"no line "<<sum<<endl;
+     else
+     	cout<< " "<<sum<<endl;
+
+     if(sum<255)
+     {
+     	if(start==0)
+     	start = i;
+ 	}
+ 	else
+ 	{
+ 		if(prev!=255&&start>0)
+ 		{
+ 			end= i-1;
+ 			if(start!=end)
+ 			{		
+ 			cout<<"Hell"<<start<<"  "<<end<<endl; 
+ 			cv::Mat imageROI;
+			imageROI= src(cv::Rect(0,start,src.cols,end));
+			cv::imshow("src",imageROI);
+  			waitKey(0);
+  		}
+ 		}
+ 		start = 0;
+ 		end=0;
+ 	}
+ 	prev=sum;	
+ }
+
+  waitKey(0);
+
+  return 0;
+}
